133041547a8f89a876495462e1c2ec9891887f31
Rafael Lima <rafael_weblima@hotmail.com>
2021-01-13 11:44:29 -0300

    Add Routers and StepsHeader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -8,13 +8,13 @@ function Footer(){
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="#" target="_new">
+                <a href="www.youtube.com" target="_new">
                     <YouTubeIcon/>
                 </a>
-                <a href="#" target="_new">
+                <a href="www.linkedin.com" target="_new">
                     <LinkedinIcon/>
                 </a>
-                <a href="#" target="_new">
+                <a href="www.instagram.com" target="_new">
                     <InstagramIcon/>
                 </a>
             </div>
