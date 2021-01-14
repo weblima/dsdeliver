13eafa886b86c03998c9ab9ac6efdcd3adb4dad4
Rafael Lima <rafael_weblima@hotmail.com>
2021-01-14 17:06:04 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+import ProductList from './ProductsList';
 import StepsHeader from './StepsHeader';
+import { Product } from './types';
 import './styles.css';
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() =>{
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    },[]);
+
     return(
         <div className="orders-container">
             <StepsHeader/>
+            <ProductList products={products}/>
+            <OrderLocation/>
         </div>
     )
 }
