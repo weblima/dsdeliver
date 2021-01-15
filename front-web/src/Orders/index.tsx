b87454a0c364bf5723ede6ec1fae5314b5b6c598
Rafael Lima <rafael_weblima@hotmail.com>
2021-01-15 11:40:59 -0300

    Save from this point

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+import ProductList from './ProductsList';
 import StepsHeader from './StepsHeader';
+import { OrderLocationdata, Product } from './types';
 import './styles.css';
+import OrderSummary from './OrderSummary';
+import Footer from '../Footer';
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+
+    useEffect(() =>{
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    },[]);
+
     return(
-        <div className="orders-container">
-            <StepsHeader/>
-        </div>
+        <>
+            <div className="orders-container">
+                <StepsHeader/>
+                <ProductList products={products}/>
+                <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
+                <OrderSummary/>
+            </div>
+            <Footer/>
+        </>
     )
 }
 
